cd04713f2768d5b2f3d0a10fd88bf2495811f17d
Carlos Illobre <carlos.illobre@rocksaucestudios.com>
2019-05-10 19:19:11 -0300

    remove promises from server

--- server.js
+++ server.js
@@ -5,38 +5,32 @@ const logger = require('./createLogger.js')()
 const createDatabase = require('./mongodb/createDatabase.js')
 const createExpressApp = require('./app/createExpressApp.js')
 
-createDatabase({ logger, mongoUri: process.env.MONGODB_URL })
-.then(database => createExpressApp({
-    logger,
-    database,
-}))
-.then(app => http
-.createServer()
-.on('request', app)
-.on('listening', function() {
-    const addr = this.address()
-    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
-    logger.info(`Listening on ${bind}`)
-})
-.on('error', function(error) {
-    if (error.syscall !== 'listen') throw error
-    const addr = this.address() || { port }
-    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
-    switch (error.code) {
-    case 'EACCES':
-        logger.error(`${bind} requires elevated privileges`)
-        process.exit(1)
-        break
-    case 'EADDRINUSE':
-        logger.error(`${bind} is already in use`)
-        process.exit(1)
-        break
-    default:
-        throw error
-    }
-})
-.listen(port)
-)
-.then(http => module.exports = http)
-.catch(error => logger.error(error))
-
+module.exports = (async () => {
+    const database = await createDatabase({ logger, mongoUri: process.env.MONGODB_URL })
+    const app = await createExpressApp({ logger, database })
+    return http.createServer()
+    .on('request', app)
+    .on('listening', function() {
+        const addr = this.address()
+        const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
+        logger.info(`Listening on ${bind}`)
+    })
+    .on('error', function(error) {
+        if (error.syscall !== 'listen') throw error
+        const addr = this.address() || { port }
+        const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
+        switch (error.code) {
+        case 'EACCES':
+            logger.error(`${bind} requires elevated privileges`)
+            process.exit(1)
+            break
+        case 'EADDRINUSE':
+            logger.error(`${bind} is already in use`)
+            process.exit(1)
+            break
+        default:
+            throw error
+        }
+    })
+    .listen(port)
+})()
